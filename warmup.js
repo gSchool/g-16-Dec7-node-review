cb326f4bf32e84eefea817f0cd32221750debee4
Tyler Bettilyon <teb311@gmail.com>
2015-12-11 10:22:45 -0800

    The live coded solution

--- warmup.js
+++ warmup.js
@@ -1,3 +1,22 @@
-function test(){
-	
+window.onload = function() {
+
+	$('#myForm').on('submit', function(event) {
+		event.preventDefault();
+		console.log(window.location);
+
+		$.ajax({
+			url: window.location, 
+			method: 'POST',
+			data: {
+				firstName: $('#firstName').val(),
+				lastName: $('#lastName').val()
+			},
+
+			success: function(data) {
+				console.log(data);
+				$('#dataHolder').append(data);
+			}
+		});
+	})
+
 }
